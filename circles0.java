ca12b457eed804a62e8139eb00aea22018f783a7
EvelynL <evelyn.r.levine@gmail.com>
2015-09-09 13:48:01 -0400

    Update circles0.java
    added fill and stroke color to ellipse, change screen size and change the width and height of ellipse to half the size of the screen

--- circles0.java
+++ circles0.java
@@ -2,10 +2,12 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(400, 400);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  fill(225,0,0);//inside circle color
+  stroke(0,0,225);//outside color
+  ellipse( width/2,height/2, 30,30 );       // Try changing the (x,y) and size.
 }
