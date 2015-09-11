1d55e3592fe9447465c777beea3b2b3791839107
EvelynL <evelyn.r.levine@gmail.com>
2015-09-11 13:06:07 -0400

    Update circles1.java
    add color,made them move

--- circles1.java
+++ circles1.java
@@ -8,15 +8,21 @@ void setup() {
   size(500, 500);        // Try changing the width & height.
   x=  width/2;
   y=  height/2;
+  background(0,0,225);
 }
 
 // Next frame:  draw three circles. //
 void draw() {
+  fill(225,0,0);//inside color
+  stroke(70,80,90);//outside color
   ellipse(x, y, 80, 80);
+  fill(0,225,0);//inside color
+  stroke(50,150,10);//outside color
   ellipse(x+60, y+60, 40, 40);
+  fill(40,30,20);//inside color
+  stroke(0,70,150);//outside color
   ellipse(x+90, y+90, 20, 20);
+ x=x+1;
+ y=y+1;
  
-  // Move them???? //--              x=  x + 2;  y=  y + 1;
-  // Insert BREAK here ^
-  // to uncomment the above code,
 }
